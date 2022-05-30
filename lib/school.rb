705ce2a5eed4d866eb3f5d780689871edf1cefaf
Malcolm Keyes <3.malcolmk@gmail.com>
2022-05-30 15:37:36 -0400

    commit tree b10d81d8664d1de44a78c238f56bee96491dc8a0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    attr_accessor :roster
+
+    def add_student(name, grade)
+        @roster[grade].nil? ? @roster[grade] = [name] : @roster[grade] << name
+    end
+
+    def grade(grade_level)
+        @roster[grade_level]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each do |grade, students|
+            sorted_roster[grade] = students.sort
+        end
+        sorted_roster
+    end
+end
